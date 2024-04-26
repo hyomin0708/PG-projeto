8cd7b9bdf0d88ed1665ecab3d64bcaed4e02eb0e
DANILO YE <144182720+yangcunwozuisao@users.noreply.github.com>
2024-04-26 01:07:52 +0000

    a

---
+++ src/main/java/ps2/restapidb/Entregador.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Entregador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String telefone;
+    private int pontuacao;
+    private boolean situacaoEntrega;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setPontuacao(int pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+
+    public boolean getSituacaoEntrega() {
+        return situacaoEntrega;
+    }
+
+    public void setSituacaoEntrega(boolean situacaoEntrega) {
+        this.situacaoEntrega = situacaoEntrega;
+    }
+}
